fd01e2fab027d876d34de061fea152893eee94c1
Esperanza Dueñas <da13002@ues.edu.sv>
2021-08-21 19:01:33 -0600

    feat: plantilla base

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
+
+// styles
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/assets/styles/tailwind.css";
+
+// mouting point for the whole app
+
+import App from "@/App.vue";
+
+// views without layouts
+
+import Landing from "@/views/Landing.vue";
+import Index from "@/views/Index.vue";
+
+// routes
+
+const routes = [
+  {
+    path: "/landing",
+    component: Landing,
+  },
+  {
+    path: "/",
+    component: Index,
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
